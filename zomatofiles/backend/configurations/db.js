269a4f009250d812bcad75d5f61e25525fbf52be
justinz-songz <2445736@students.wits.ac.za>
2025-02-20 23:17:13 +0200

    Updates

--- zomatofiles/backend/configurations/db.js
+++
@@ -1,9 +0,0 @@
-//logic that conects to the db
-import mongoose from "mongoose";
-
-
-export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://Justin02:Jordinspark$02@atlascluster.r6dcxci.mongodb.net/zomato').then(()=>{
-        console.log("DB connected")
-    })
-}
